6b3dae03b5bcbb98df92e0528f9db6de37e653b6
James Paterson <j@presstube.com>
2016-06-10 11:02:26 -0400

    blocking in markup

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,31 @@ export default class App extends React.Component {
   render () {
     return (
       <div>
-        Hello, world! I am an app. yay!!!
+      
+        <h1>
+          Bright, spacious studio in the heart of Mile End
+        </h1>
+
+        <p>
+          Share a space with 3 friendly, nerdy creative professionals.
+        </p>
+
+        <p>
+          $500/m for an X sq foot space large enough for two.
+        </p>
+
+        <p>
+          Email James: <a href="mailto:j@presstube.com">j@presstube.com</a>
+          <br/>
+          or
+          <br/>
+          Phone Luc: 514.424.2363
+        </p>
+
+        <img src="assets/corner.jpg"/>
+        <img src="assets/kitchen.jpg"/>
+        <img src="assets/center.jpg"/>
+        <img src="assets/exterior.jpg"/>
       </div>
     )
   }
