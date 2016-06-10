645238311f9d412ec1ddbda0e8108d61165dd598
Jason Kuhrt <jasonkuhrt@me.com>
2016-06-10 11:36:20 -0400

    Monospace and grey.

--- src/components/app.js
+++ src/components/app.js
@@ -10,28 +10,26 @@ export default class App extends React.Component {
           textAlign: "center"
         }}
       >
-
-        <div>
+        <div className="header">
+          <h1>
+            Bright, spacious studio in the heart of Mile End
+          </h1>
         </div>
-        <h1>
-          Bright, spacious studio in the heart of Mile End
-        </h1>
+        <div className="description">
+          <p>
+            Share a space with 3 friendly, nerdy creative professionals.
+          </p>
+          <p>
+            Located at the corner of Saint Laurent & Saint Viateur.
+          </p>
 
-        <p>
-          Share a space with 3 friendly, nerdy creative professionals.
-        </p>
-
-        <p>
-          Located at the corner of Saint Laurent & Saint Viateur.
-        </p>
-
-        <p>
-          $500/m for a space large enough to comfortably fit 2.
-        </p>
-
-        <p>
-          Email James: <a href="mailto:j@presstube.com">j@presstube.com</a>, or phone Luc: 514.424.2363.
-        </p>
+          <p>
+            $500/m for a space large enough to comfortably fit 2.
+          </p>
+          <p>
+            Email James: <a href="mailto:j@presstube.com">j@presstube.com</a>, or phone Luc: 514.424.2363.
+          </p>
+        </div>
 
         <div>
           <img src="assets/corner.jpg"/>
